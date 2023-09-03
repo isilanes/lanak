d1b56757436a9b5012193d2f4a4afd38cc5aaa78
Iñaki Silanes <isilanes@gmail.com>
2023-09-03 22:21:01 +0200

    Started adding async read from example in flutter.dev

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -10,12 +10,14 @@ class MainView extends StatefulWidget {
   final String title = "Lanak list";
 
   @override
-  State<MainView> createState() => _MainViewState();
+  // State<MainView> createState() => _MainViewState();
+  State<MainView> createState() => _FutureBuilderExampleState();
 }
 
 
 class _MainViewState extends State<MainView> {
   final styleTaskListText = const TextStyle(fontSize: 24);
+  final Future<List<String>> _tasks = getTasks();
 
   void _addTask() {
     Navigator.push(
@@ -54,6 +56,50 @@ class _MainViewState extends State<MainView> {
     return ListTile(
       leading: const Icon(Icons.access_time, color: Colors.deepOrange),
       title: Text(task, style: styleTaskListText,),
+    );
+  }
+}
+
+
+class _FutureBuilderExampleState extends State<MainView> {
+  final Future<List<String>> _nameList = Future<List<String>>.delayed(
+      const Duration(seconds: 10),
+        () => ["good", "bad"],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      style: Theme.of(context).textTheme.displayMedium!,
+      textAlign: TextAlign.center,
+      child: FutureBuilder<List<String>>(
+        future: _nameList, // a previously-obtained Future<String> or null
+        builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+          List<Widget> children;
+          if (snapshot.hasData) {
+            children = <Widget>[
+              for (final task in snapshot.data!)
+                Text(task)
+            ];
+          } else if (snapshot.hasError) {
+            children = <Widget>[
+              const Text("error"),
+              const Text("death"),
+            ];
+          } else {
+            children = <Widget>[
+              const Text("waiting for"),
+              const Text("taskList"),
+            ];
+          }
+          return Center(
+              child: ListView(
+                  padding: const EdgeInsets.all(16.0),
+                  children: children,
+              )
+          );
+        },
+      ),
     );
   }
 }
