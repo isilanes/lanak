b04116077e4d2300a13efe358252a51cb871b70d
Iñaki Silanes <isilanes@gmail.com>
2024-02-11 20:49:03 +0100

    Add header with some summary to main view

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -5,6 +5,27 @@ import 'package:lanak/views/lana_edit.dart';
 import 'package:lanak/views/lana_detail.dart';
 
 
+class TaskLoad {
+  double nTasks;
+  double totalHours;
+
+  TaskLoad({
+    this.nTasks = 0,
+    this.totalHours = 0,
+  });
+
+  void reset() {
+    nTasks = 0;
+    totalHours = 0;
+  }
+
+  void addTask(Map<String, dynamic> task) {
+    nTasks += 1;
+    totalHours += task["hours"] ?? 0;
+  }
+}
+
+
 class MainView extends StatefulWidget {
   const MainView({super.key});
 
@@ -18,6 +39,7 @@ class MainView extends StatefulWidget {
 class _MainViewState extends State<MainView> {
   final styleTaskListText = const TextStyle(fontSize: 24, color: Colors.white);
   Future<List<Map<String, dynamic>>> _tasks = getLanakWithLag();
+  TaskLoad taskLoad = TaskLoad();
 
   void _goToLanaAdd() async {
     await Navigator.push(
@@ -39,6 +61,19 @@ class _MainViewState extends State<MainView> {
     setState(() {});
   }
 
+  Widget _titleRow() {
+    const Color color = Colors.blueAccent;
+    const styleTitle = TextStyle(fontSize: 20, color: color);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Text(
+        "${taskLoad.nTasks} tasks, ${taskLoad.totalHours} total hours",
+        style: styleTitle,
+      )
+    );
+  }
+
   Widget _buildRow(Map<String, dynamic> task) {
     final screen = MediaQuery.of(context).size;
     final lag = task["lag"];
@@ -46,6 +81,7 @@ class _MainViewState extends State<MainView> {
     if (lag < 0) {
       color = Colors.lightGreen;
     }
+    taskLoad.addTask(task);
 
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
@@ -90,34 +126,46 @@ class _MainViewState extends State<MainView> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: FutureBuilder<List<Map<String, dynamic>>>(
-        future: _tasks,
-        builder: (
-            BuildContext context,
-            AsyncSnapshot<List<Map<String, dynamic>>> snapshot,
-        ) {
-          List<Widget> children;
-          if (snapshot.hasData) {
-            children = <Widget>[
-              for (final task in snapshot.data!)
-                _buildRow(task)
-            ];
-          } else if (snapshot.hasError) {
-            children = <Widget>[
-              const Text("error"),
-              const Text("death"),
-            ];
-          } else {
-            children = <Widget>[
-              const Text("waiting for"),
-              const Text("taskList"),
-            ];
-          }
-          return ListView(
-              padding: const EdgeInsets.all(16.0),
-              children: children,
-          );
-        }
+      body: Column(
+        children: <Widget>[
+          _titleRow(),
+          FutureBuilder<List<Map<String, dynamic>>>(
+              future: _tasks,
+              builder: (
+                  BuildContext context,
+                  AsyncSnapshot<List<Map<String, dynamic>>> snapshot,
+              ) {
+                List<Widget> children;
+                taskLoad.reset();
+                // taskLoad.nTasks = 0;
+                // taskLoad.totalHours = 0;
+                if (snapshot.hasData) {
+                  children = <Widget>[
+                    for (final task in snapshot.data!)
+                      _buildRow(task)
+                  ];
+                } else if (snapshot.hasError) {
+                  children = <Widget>[
+                    const Text("error"),
+                    const Text("death"),
+                  ];
+                } else {
+                  children = <Widget>[
+                    const Text("waiting for"),
+                    const Text("taskList"),
+                  ];
+                }
+                return Expanded(
+                    child: ListView(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.all(16.0),
+                    children: children,
+                  )
+                );
+              }
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _goToLanaAdd,
